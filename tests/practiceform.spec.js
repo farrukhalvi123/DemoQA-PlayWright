4e28fce1741c715dc117d854435fd16d74836511
Farrukh123 <PMXSEePs4hXRPrBm28DJ>
2025-02-14 00:48:28 +0500

    POM Implemented

--- tests/practiceform.spec.js
+++ tests/practiceform.spec.js
@@ -1,32 +1,24 @@
-import { test, expect } from '@playwright/test'
+import { test, expect } from '@playwright/test';
+import { Practicepage } from '../Pages/PracticeformPages';
 import * as path from 'path';
 
 test.beforeEach(async ({ page }) => {
-    await page.goto("https://demoqa.com/")
-    await expect(page).toHaveTitle("DEMOQA")
-
-})
+    await page.goto("https://demoqa.com/");
+    await expect(page).toHaveTitle("DEMOQA");
+});
 
 test.afterEach(async ({ page }) => {
-    await page.close()
+    await page.close();
+});
 
-})
-test("Practice form test", async ({ page, context }) => {
-
-    await page.locator("//div[@class='home-body']//div[2]//div[1]//div[3]").click();
-    await page.locator("//div[@class='element-list collapse show']//li[@id='item-0']").click();
-    const element = page.locator('h1');
-    await expect(element).toHaveText('Practice Form');
-    await page.locator("//input[@id='firstName']").fill("FJ")
-    await page.locator("//input[@id='lastName']").fill("FJA")
-    await page.locator("//input[@id='userEmail']").fill("fja@yopmail.com")
-    const phonefield = await page.locator("//input[@id='userNumber']")
-    phonefield.fill("1236547890")
-    await page.click("//label[normalize-space()='Male']")
-    await page.locator("//input[@id='dateOfBirthInput']").fill('2025-02-01')
+test("Practice form test", async ({ page }) => {
+    const prac = new Practicepage(page);
+    await prac.go_to_practiceform();
+     
     const cwd = process.cwd();
-    const fpath = path.join(cwd, "Data Flow Diagram Whiteboard in Red Light Red Monochromatic Style (2).png")
-    await page.locator("//input[@id='uploadPicture']").setInputFiles(fpath)
-    await page.click("//button[@id='submit']")
+    const fpath = path.join(cwd, "Data Flow Diagram Whiteboard in Red Light Red Monochromatic Style (2).png");
 
-})
+    await prac.enter_textfields("farrukh", "alvi", "fja@yopmail.com", "1234567890", "2025-02-01", fpath);
+    await prac.clickers();
+    await page.pause()
+});
